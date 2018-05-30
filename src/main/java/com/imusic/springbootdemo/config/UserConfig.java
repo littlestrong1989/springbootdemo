13f1ca451dfacbd0ab8974e038b1fa0f390700b0
littlestrong1989 <you@example.com>
2018-05-30 17:46:34 +0800

    modify

--- src/main/java/com/imusic/springbootdemo/config/UserConfig.java
+++ src/main/java/com/imusic/springbootdemo/config/UserConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import com.imusic.springbootdemo.entity.User;
 
 @Configuration
+
 public class UserConfig {
 
 	@Bean
--- src/main/java/com/imusic/springbootdemo/config/controller/HomeController.java
+++ src/main/java/com/imusic/springbootdemo/config/controller/HomeController.java
@@ -1,10 +1,8 @@
-package com.imusic.springbootdemo.config22.controller;
+package com.imusic.springbootdemo.config.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.imusic.springbootdemo.entity.User;
 
 @Controller
@@ -14,12 +12,6 @@ public class HomeController {
 	private User user;
 	@RequestMapping(value="/springboot/index")
 	public String index(){
-		System.out.println(user);
-		return "index";
-	}
-	
-	@RequestMapping(value="/springboot/index2")
-	public String index2(){
 		System.out.println(user);
 		return "index";
 	}
